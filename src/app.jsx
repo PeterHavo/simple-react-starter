a8496e4da8358e147dabbb9b0b21575d44df1de9
Mario Uhrin <mario.uhrin@gmail.com>
2017-08-26 12:49:01 +0200

    add all files

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import '../styles/index.scss'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World !</h1>
+				<h3>Let's try to create something</h3>
+      </div>
+
+    )
+  }
+}
